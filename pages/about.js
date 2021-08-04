427990514d13974ed55a44a5d64443e25474316e
ramon-poetic <ramon@poetic.io>
2021-08-03 22:56:43 -0500

    created consistant navbar

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+
+export default function About() {
+  return (
+    <div className="">
+      <Head>
+        <title>About</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="">
+        <h1 className="">About</h1>
+      </main>
+    </div>
+  )
+}
